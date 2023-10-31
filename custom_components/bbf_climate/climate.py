ca45fcf6a7e0e1ec547bafcf7050f18436f729d0
nakata5321 <makar5321@gmail.com>
2023-10-31 10:44:43 +0200

    migrate to paho mqtt

--- custom_components/bbf_climate/climate.py
+++ custom_components/bbf_climate/climate.py
@@ -1,4 +1,7 @@
+import asyncio
 import logging
+import time
+from random import randint
 
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
@@ -17,10 +20,18 @@ from homeassistant.const import (
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
-from homeassistant.components.mqtt.client import publish as mqtt_publish
-from homeassistant.components.mqtt.client import subscribe as mqtt_subscribe
+from .utils import to_thread
+
+from paho.mqtt import client as mqtt_client
+from homeassistant.components.mqtt.const import DATA_MQTT
 
 _LOGGER = logging.getLogger(__name__)
+
+# set mqtt parameters
+FIRST_RECONNECT_DELAY = 1
+RECONNECT_RATE = 2
+MAX_RECONNECT_COUNT = 12
+MAX_RECONNECT_DELAY = 60
 
 DOMAIN = "bbf_climate"
 DEFAULT_NAME = "BBF Climate"
@@ -48,35 +59,100 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 class BbfClimate(ClimateEntity):
 
     def __init__(self, hass: HomeAssistant, config: ConfigType):
+        # Set Climate attributes
         super().__init__()
         self._attr_has_entity_name = True
         self._attr_name = config[CONF_NAME]
         self.set_topic = config[CONF_MQTT_SET_TOPIC]
         self.get_topic = config[CONF_MQTT_GET_TOPIC]
-
-        self.hass = hass
         self._attr_temperature_unit = hass.config.units.temperature_unit
         self._attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE
         self._attr_min_temp = 16
         self._attr_max_temp = 26
-
         self._attr_hvac_mode = HVACMode.OFF
         self._attr_hvac_modes = [
                 HVACMode.OFF,
                 HVACMode.COOL,
                 HVACMode.HEAT,
             ]
-
         self._attr_target_temperature = 21
 
-        self.subscriber = mqtt_subscribe(self.hass, self.set_topic, self.update)
+        self.hass = hass
 
+        # get information about MQTT client
+        self.mqtt_data = self.hass.data[DATA_MQTT].client.conf
+        self.mqtt_broker = self.mqtt_data["broker"]
+        self.mqtt_port = self.mqtt_data["port"]
+        self.mqtt_client_id = f'bbf-climate-{randint(0, 1000)}'
+        self.mqtt_username = self.mqtt_data["username"]
+        self.mqtt_password = self.mqtt_data["password"]
+
+        # Create mqtt client
+        self.mqtt_client = self.connect_mqtt()
+        self.mqtt_client.loop_start()
+
+    def on_disconnect(self, client, userdata, rc):
+        _LOGGER.warning("Disconnected with result code: %s", rc)
+        reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
+        while reconnect_count < MAX_RECONNECT_COUNT:
+            _LOGGER.info("Reconnecting in %d seconds...", reconnect_delay)
+            time.sleep(reconnect_delay)
+
+            try:
+                client.reconnect()
+                _LOGGER.info("Reconnected successfully!")
+                return
+            except Exception as err:
+                _LOGGER.error("%s. Reconnect failed. Retrying...", err)
+
+            reconnect_delay *= RECONNECT_RATE
+            reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
+            reconnect_count += 1
+        _LOGGER.info("Reconnect failed after %s attempts. Exiting...", reconnect_count)
+
+    def connect_mqtt(self):
+        def on_connect(client, userdata, flags, rc):
+            if rc == 0:
+                _LOGGER.info("Connected to MQTT Broker!")
+            else:
+                _LOGGER.warning("Failed to connect, return code %d\n", rc)
+
+        # Set Connecting Client ID
+        client = mqtt_client.Client(self.mqtt_client_id)
+        client.username_pw_set(self.mqtt_username, self.mqtt_password)
+        client.on_connect = on_connect
+        client.on_disconnect = self.on_disconnect
+        client.connect(self.mqtt_broker, self.mqtt_port)
+        return client
+
+    @to_thread
+    def mqtt_publish(self, client: mqtt_client.Client, topic: str, msg: str) -> None:
+        msg_count = 1
+        while True:
+            time.sleep(1)
+            result = client.publish(topic, msg)
+            # result: [0, 1]
+            status = result[0]
+            if status == 0:
+                _LOGGER.info(f"Send `{msg}` to topic `{topic}`")
+                break
+            else:
+                _LOGGER.warning(f"Failed to send message to topic {topic}")
+            msg_count += 1
+            if msg_count > 5:
+                _LOGGER.error(f"unable to sent message {msg} to topic {topic}")
+                break
+
+
+
+
+    # Climate set methods
     def set_hvac_mode(self, hvac_mode):
         """Set new target hvac mode."""
         self._attr_hvac_mode = hvac_mode
         self.async_write_ha_state()
 
-        mqtt_publish(hass=self.hass, topic=self.set_topic, payload=f"set new hvac mode - {self.hvac_mode}")
+        asyncio.ensure_future(self.mqtt_publish(client=self.mqtt_client, topic=self.set_topic, msg=f"set new hvac mode - {self.hvac_mode}"))
 
     def set_temperature(self, **kwargs):
         """Set new target temperature."""
@@ -84,7 +160,7 @@ class BbfClimate(ClimateEntity):
         self._attr_target_temperature = kwargs.get(ATTR_TEMPERATURE)
         self.async_write_ha_state()
 
-        mqtt_publish(hass=self.hass, topic=self.get_topic, payload=f"set new temperature mode - {self.target_temperature}")
+        asyncio.ensure_future(self.mqtt_publish(client=self.mqtt_client, topic=self.get_topic, msg=f"set new temperature mode - {self.target_temperature}"))
 
     def update(self, topic, payload, qos) -> None:
         _LOGGER.info(f"i've get value {payload}")
---
+++ custom_components/bbf_climate/utils.py
@@ -0,0 +1,10 @@
+import typing as tp
+import functools
+import asyncio
+
+
+def to_thread(func: tp.Callable) -> tp.Coroutine:
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        return await asyncio.to_thread(func, *args, **kwargs)
+    return wrapper
