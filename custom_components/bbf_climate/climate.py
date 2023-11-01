655caf204e88300d82ea900c4468d92245c41652
nakata5321 <makar5321@gmail.com>
2023-11-01 16:28:42 +0200

    version 0.1.0

--- custom_components/bbf_climate/climate.py
+++ custom_components/bbf_climate/climate.py
@@ -2,16 +2,18 @@ import logging
 import time
 from random import randint
 
+
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.event import async_track_state_change
 import voluptuous as vol
 from homeassistant.components.climate import ClimateEntity
 from homeassistant.components.climate.const import HVACMode, ClimateEntityFeature
 
-from homeassistant.const import CONF_NAME, ATTR_TEMPERATURE
-from homeassistant.core import HomeAssistant
+from homeassistant.const import CONF_NAME, ATTR_TEMPERATURE, STATE_UNKNOWN, STATE_UNAVAILABLE
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.typing import ConfigType
 
-from .utils import convert_temp_from_dex
+from .utils import convert_temp_from_hex, convert_temp_to_hex
 
 from paho.mqtt import client as mqtt_client
 from homeassistant.components.mqtt.const import DATA_MQTT
@@ -34,10 +36,14 @@ CONF_MQTT_GET_TOPIC = "get_topic"
 DEFAULT_TEMP = 21
 DEFAULT_PRECISION = 1.0
 
+
+INELS_HVAC_MODES = {"off": "00", "heat": "01", "cool": "03"}
+
+
 PLATFORM_SCHEMA = cv.PLATFORM_SCHEMA.extend(
     {
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-        vol.Required(CONF_TEMPERATURE_ENTITY_ID): cv.string,
+        vol.Required(CONF_TEMPERATURE_ENTITY_ID): cv.entity_id,
         vol.Required(CONF_MQTT_SET_TOPIC): cv.string,
         vol.Required(CONF_MQTT_GET_TOPIC): cv.string,
     }
@@ -57,7 +63,7 @@ class BbfClimate(ClimateEntity):
 
         self._attr_has_entity_name = True
         self._attr_name = config[CONF_NAME]
-        self.temp_sensor = config[CONF_TEMPERATURE_ENTITY_ID]
+        self._temperature_sensor = config[CONF_TEMPERATURE_ENTITY_ID]
         self.set_topic = config[CONF_MQTT_SET_TOPIC]
         self.get_topic = config[CONF_MQTT_GET_TOPIC]
         self._attr_temperature_unit = hass.config.units.temperature_unit
@@ -88,14 +94,36 @@ class BbfClimate(ClimateEntity):
         self.mqtt_client.loop_start()
 
         # callback values
-        self.actual_temp_raw = ""
-        self.critical_temp_raw = hex(50)
+        self.critical_temp_raw = convert_temp_to_hex(50)
 
-    def async_update(self):
-        temp = self.hass.states.get(self.temp_sensor).attributes["temperature"]
-        _LOGGER.info(self.hass.states.get(self.temp_sensor).attributes)
-        self._attr_current_temperature = temp
+    async def async_added_to_hass(self):
+        """Run when entity about to be added."""
+        await super().async_added_to_hass()
+        _LOGGER.debug(f"async_added_to_hass {self} {self.name} {self.supported_features}")
+
+        if self._temperature_sensor:
+            async_track_state_change(self.hass, self._temperature_sensor, self._async_temp_sensor_changed)
+
+    async def _async_temp_sensor_changed(self, entity_id, old_state, new_state):
+        """Handle temperature sensor changes."""
+        if new_state is None:
+            return
+
+        self._async_update_temp(new_state)
         self.async_write_ha_state()
+        _LOGGER.info(f"_async_temp_sensor_changed")
+
+        output_message = self.create_msg()
+        self.mqtt_publish(client=self.mqtt_client, topic=self.set_topic, msg=output_message)
+
+    @callback
+    def _async_update_temp(self, state):
+        """Update thermostat with the latest state from temperature sensor."""
+        try:
+            if state.state != STATE_UNKNOWN and state.state != STATE_UNAVAILABLE:
+                self._attr_current_temperature = float(state.state)
+        except ValueError as ex:
+            _LOGGER.error("Unable to update from temperature sensor: %s", ex)
 
     def on_disconnect(self, client, userdata, rc):
         _LOGGER.warning("Disconnected with result code: %s", rc)
@@ -134,7 +162,6 @@ class BbfClimate(ClimateEntity):
     def mqtt_publish(self, client: mqtt_client.Client, topic: str, msg: str) -> None:
         msg_count = 1
         while True:
-            time.sleep(1)
             result = client.publish(topic, msg)
             # result: [0, 1]
             status = result[0]
@@ -147,22 +174,17 @@ class BbfClimate(ClimateEntity):
             if msg_count > 5:
                 _LOGGER.error(f"unable to sent message {msg} to topic {topic}")
                 break
+            time.sleep(1)
 
     def on_message(self, client: mqtt_client.Client, userdata, msg):
         _LOGGER.info(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
         message = msg.payload.decode()
 
-        # TODO check with var are needed
-        actual_temp = convert_temp_from_dex(message, 0, 4)
-        required_therm_term = convert_temp_from_dex(message, 8, 12)
-        required_ac_term = convert_temp_from_dex(message, 20, 24)
-        living_correction = convert_temp_from_dex(message, 24, 28)
-
-        self.actual_temp_raw = message.split("\n")[0:4]
-        self.actual_temp_raw = "\n".join(self.actual_temp_raw)
+        required_therm_term = convert_temp_from_hex(message, 8, 12)
+        required_ac_term = convert_temp_from_hex(message, 20, 24)
 
         value_on_off = message.split("\n")[30:31]
-        value_on_off = bin(int(value_on_off, 16))[2:].zfill(4)
+        value_on_off = bin(int(value_on_off[0], 16))[2:].zfill(8)[4:]
         if value_on_off[3] == "0":
             self._attr_hvac_mode = HVACMode.OFF
         else:
@@ -183,27 +205,31 @@ class BbfClimate(ClimateEntity):
 
         self.async_write_ha_state()
 
+    def create_msg(self) -> str:
+        # create output message
+        message = ""
+        actual_temp_raw = convert_temp_to_hex(self.current_temperature)
+        message = message + actual_temp_raw + "\n" + self.critical_temp_raw  # data 0-7
+        current_temp = convert_temp_to_hex(self.target_temperature)
+        message = message + "\n" + current_temp + "\n" + current_temp  # data 8-15
+        message = message + "\n00\n07"  # data 16-17
+
+        message = message + "\n" + INELS_HVAC_MODES[self.hvac_mode]
+        return message
+
     # Climate set methods
     def set_hvac_mode(self, hvac_mode):
         """Set new target hvac mode."""
         self._attr_hvac_mode = hvac_mode
         self.async_write_ha_state()
-        message = ""
 
-        self.mqtt_publish(
-            client=self.mqtt_client,
-            topic=self.set_topic,
-            msg=f"set new hvac mode - {self.hvac_mode}",
-        )
+        output_message = self.create_msg()
+        self.mqtt_publish(client=self.mqtt_client, topic=self.set_topic, msg=output_message)
 
     def set_temperature(self, **kwargs):
         """Set new target temperature."""
-
         self._attr_target_temperature = kwargs.get(ATTR_TEMPERATURE)
         self.async_write_ha_state()
 
-        self.mqtt_publish(
-            client=self.mqtt_client,
-            topic=self.get_topic,
-            msg=f"set new temperature mode - {self.target_temperature}",
-        )
+        output_message = self.create_msg()
+        self.mqtt_publish(client=self.mqtt_client, topic=self.set_topic, msg=output_message)
--- custom_components/bbf_climate/utils.py
+++ custom_components/bbf_climate/utils.py
@@ -1,4 +1,7 @@
-def convert_temp_from_dex(message: str, start_num: int, finish_num: int) -> float:
+from textwrap import wrap
+
+
+def convert_temp_from_hex(message: str, start_num: int, finish_num: int) -> float:
     """
     convert hex byte value to float number
 
@@ -9,6 +12,24 @@ def convert_temp_from_dex(message: str, start_num: int, finish_num: int) -> floa
     """
     hex_number = message.split("\n")[start_num:finish_num]
     hex_number = hex_number[::-1]
-    dex_number = int("".join(hex_number))
+    dex_number = int("".join(hex_number), 16)
 
-    return float(dex_number/100)
+    return float(dex_number / 100)
+
+
+def convert_temp_to_hex(number: float) -> str:
+    """
+
+    :param number: number to convert to hex
+    :rtype: return string with formatted hex value
+    """
+    dex_number = int(number * 100)
+    hex_number = hex(dex_number)[2:]  # delete 'ox' part
+    # output value should be 8 char. so we should add '0' to the beginning of str
+    add_len = 8 - len(hex_number)
+    for i in range(add_len):
+        hex_number = "0" + hex_number
+    hex_arr = wrap(hex_number, 2)
+    hex_arr.reverse()
+
+    return "\n".join(hex_arr)
